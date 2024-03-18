a35629bb07bad57e3d7bf5e1d296ba0e567e81c6
Fok Chi Hin Nestor <chi.fok@student.manchester.ac.uk>
2024-03-18 08:51:45 +0800

    uncomment test

--- HKEXscraper2/backtest_yearly/main.py
+++ HKEXscraper2/backtest_yearly/main.py
@@ -33,8 +33,8 @@ if __name__ == "__main__":
 
     print("______________start______________")
 
-    # strategy.strategy(start_date='01-01-2018', end_date='31-12-2018')
-    # portfolio.set_balance(100)
+    strategy.strategy(start_date='01-01-2018', end_date='31-12-2018')
+    portfolio.set_balance(100)
 
     print("_____________end________________")
 
